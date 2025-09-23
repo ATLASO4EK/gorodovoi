779012b9f98f28f867063fba74cdd7843c4a2fb5
AtLaS <atlaso4ek@gmail.com>
2025-09-23 20:10:12 +0700

    Add filters to getFinesStats()

--- src/Database/getmethods.py
+++ src/Database/getmethods.py
@@ -41,23 +41,38 @@ def getFinesStats(date:datetime.date=None,
                   got_maxnum:float=None):
     conn, cur = connect()
 
-    if date is None:
-        query = "SELECT * FROM city_ops.fines"
-        cur.execute(query)
-        data = cur.fetchall()
-
-        return data
-
-    query = f"SELECT * FROM city_ops.fines WHERE report_date = '{date.isoformat()}'"
-
-    if date_end is not None:
+    # Составление запроса + фильтры
+    if date is not None and date_end is not None:
         query = (f"SELECT * FROM city_ops.fines WHERE report_date >= '{date.isoformat()}' "
                  f"AND report_date <= '{date_end}'")
+    elif date is not None:
+        query = f"SELECT * FROM city_ops.fines WHERE report_date = '{date.isoformat()}' "
+    else:
+        query = "SELECT * FROM city_ops.fines WHERE collected_sum_cum > -100"
 
+    if cam_lownum is not None:
+        query += f"AND cams_violations_cum >= {cam_lownum} "
 
+    if cam_maxnum is not None:
+        query += f"AND cams_violations_cum <= {cam_maxnum} "
 
+    if dec_lownum is not None:
+        query += f"AND decisions_cum >= {dec_lownum} "
 
+    if dec_maxnum is not None:
+        query += f"AND decisions_cum <= {dec_lownum} "
 
+    if fines_lownum is not None:
+        query += f"AND fines_sum_cum >= {fines_lownum} "
+
+    if fines_maxnum is not None:
+        query += f"AND fines_sum_cum <= {fines_maxnum} "
+
+    if got_minnum is not None:
+        query += f"AND collected_sum_cum >= {got_minnum} "
+
+    if got_maxnum is not None:
+        query += f"AND collected_sum_cum <= {got_maxnum} "
 
     cur.execute(query)
     data = cur.fetchall()
