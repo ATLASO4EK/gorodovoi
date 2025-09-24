c0b81c8eea95f6d2472cd33b583d454c03a869d9
AtLaS <atlaso4ek@gmail.com>
2025-09-24 20:44:20 +0700

    add SQL-requests for news (needs hotfix)

---
+++ src/Database/get_news.py
@@ -0,0 +1,56 @@
+import datetime
+from src.Database.connection import *
+
+def getLastNews():
+    """
+    Возвращает из SQL-таблицы данные о 4 последних новостях
+    :return:
+    """
+    conn, cur = connect()
+
+    query = 'SELECT * FROM public.news ORDER BY datetime DESC LIMIT 4'
+
+    try:
+        cur.execute(query)
+        news = cur.fetchall()
+    except Exception as e:
+        print(e)
+        return False, e
+
+    return True, news
+
+def getLastNews_tg():
+    """
+    Возвращает из SQL-таблицы данные о 2 последних новостях без развернутого текста
+    :return:
+    """
+    conn, cur = connect()
+
+    query = 'SELECT author, header, short_text FROM public.news ORDER BY datetime DESC LIMIT 2'
+
+    try:
+        cur.execute(query)
+        news = cur.fetchall()
+    except Exception as e:
+        print(e)
+        return False, e
+
+    return True, news
+
+def getFullNews():
+    """
+    Возвращает из SQL-таблицы данные о всех новостях
+    :return:
+    """
+    conn, cur = connect()
+
+    query = 'SELECT * FROM public.news'
+
+    try:
+        cur.execute(query)
+        news = cur.fetchall()
+    except Exception as e:
+        print(e)
+        return False, e
+
+    return True, news
