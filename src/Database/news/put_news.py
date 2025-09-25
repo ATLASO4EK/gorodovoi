b6bcbef19f5a063255e3c466553a79ff6689b0f3
AtLaS <atlaso4ek@gmail.com>
2025-09-25 13:27:20 +0700

    Add SQL-method update for news (search by id)

--- src/Database/news/put_news.py
+++ src/Database/news/put_news.py
@@ -1,8 +1,38 @@
+import datetime
+
 from src.Database.connection import *
 
-def putNews():
+def putNews(id:int,
+            date:datetime.datetime,
+            author:str,
+            header:str,
+            short_text:str,
+            full_text:str,
+            image:str):
     """
     Изменяет строку в БД с новостями
     :return:
     """
-    pass
+
+    conn, cur = connect()
+
+    data = (date, author, header, short_text, full_text, image, id)
+    query = ("UPDATE public.news "
+             "SET "
+             "datetime = %s, "
+             "author = %s, "
+             "header = %s, "
+             "short_text = %s, "
+             "full_text = %s, "
+             "image = %s "
+             "WHERE "
+             "id = %s")
+
+    try:
+        cur.execute(query, data)
+        conn.commit()
+        return True
+
+    except Exception as e:
+        print(e)
+        return e
