33cb19e63d1ba5dd7fac7a616e32605300f288bc
Bitt.js <118621206+Bittjs@users.noreply.github.com>
2025-09-25 10:31:46 +0700

    save just to be sure

--- src/smolensk-codd/src/App.jsx
+++ src/smolensk-codd/src/App.jsx
@@ -1,3 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
+
 import React, { useState } from 'react';
 import Header from "./Header";
 import './App.css';
