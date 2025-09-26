f79e7f835a40eca2450133b4b7de58c1691c8a6c
AtLaS <atlaso4ek@gmail.com>
2025-09-26 19:28:21 +0700

    randomforest ml model done

--- src/ML/jams_pred/data_gen.py
+++ src/ML/jams_pred/data_gen.py
@@ -12,8 +12,10 @@ rushhours = [7,8,9,17,18,19] # Час-пик
 unrushhours = [0,1,2,3,4,5,6,11,14,23] # Сон-час
 data = []
 
-for i in range(2500):
+for i in range(100000):
     k = 1.0
+    min = 1
+    max = 3
     date = fake.date_time_ad(start_datetime=datetime.date(2025, 1,1), end_datetime=datetime.date(2025,12,31))
     weekday = date.weekday()
     weekday_out = np.eye(7)[weekday]
@@ -22,26 +24,26 @@ for i in range(2500):
     holiday = 0
     # Проверяем на выходной
     if m_d in holidays:
-        rest_day = 1
-        holiday = 1
-    if weekday == 7 or weekday == 1:
-        rest_day = 1
+        min = 3
+        max = 5
 
     # Если конец недели коэф-т пробок выше
     if weekday == 6 or weekday == 7 or weekday == 1:
-        k+=0.1
+        min = 3
+        max = 5
 
     # Если час-пик и выходной или рабочий день коэф-т выше
     if date.hour in rushhours:
-        k+=0.3
-
+        min = 6
+        max = 10
     #Если сон-час коэф-т значительно ниже
     if date.hour in unrushhours:
-        k-=0.4
+        min = 1
+        max = 3
 
-    label = round(randint(1, 7)*k)/10
+    label = round(randint(min, max)*k)/10
 
-    data.append([date.hour/23, weekday_out[0], weekday_out[1], weekday_out[2],
+    data.append([round(date.hour/23, 2), weekday_out[0], weekday_out[1], weekday_out[2],
                 weekday_out[3], weekday_out[4], weekday_out[5], weekday_out[6], holiday, label])
 
 df = pd.DataFrame(columns=['hour', 'isMonday', 'isTuesday', 'isWednesday',
--- src/ML/jams_pred/random_forest_train.py
+++ src/ML/jams_pred/random_forest_train.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import accuracy_score
+import pickle
 
 import pandas as pd
 
@@ -11,14 +12,11 @@ y_data = df.iloc[:,9]
 
 x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.3, random_state=33)
 
-model = RandomForestRegressor(n_estimators=100, random_state=33)
+model = RandomForestRegressor(n_estimators=50, random_state=33)
 
 model.fit(x_train, y_train)
 
-out = np.around(model.predict(x_test), 1)
-equality = out == y_test
-
-df1 = pd.DataFrame(data = equality)
-
 accuracy = model.score(x_test, y_test)
 print(f"Точность модели: {accuracy:3f}")
+
+pickle.dump(model, open('rand_forest[0.86].pickle', 'wb'))
