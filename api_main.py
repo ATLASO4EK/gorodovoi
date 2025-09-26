564831baed469459d7502cae0f902c09d71c99e7
AtLaS <atlaso4ek@gmail.com>
2025-09-26 21:12:16 +0700

    add API-methods get for ML-analytics

--- api_main.py
+++ api_main.py
@@ -5,6 +5,7 @@ from src.API.public_info_handlers.handlers_get import *
 from src.API.public_info_handlers.handlers_post import *
 from src.API.public_info_handlers.handlers_put import *
 from src.API.private_info_handlers.handlers_tg import *
+from src.API.ML_handlers.ML_handlers import *
 from src.API.import_export_api import *
 
 
