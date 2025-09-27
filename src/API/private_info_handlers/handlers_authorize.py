6117e85e8887dc456b05d3c62e7e926752927403
AtLaS <atlaso4ek@gmail.com>
2025-09-27 16:06:02 +0700

    add authorize api-methods

---
+++ src/API/private_info_handlers/handlers_authorize.py
@@ -0,0 +1,30 @@
+from src.API.app import app
+from flask import  request, jsonify
+
+from src.config import getAdm, getEditor
+
+# Функция входа
+@app.route('/api/v1/Auth', methods=['GET'])
+def getAuth_api():
+    try:
+        site = request.args.get('site')
+        code = request.args.get('code')
+    except Exception as e:
+        return jsonify([e]), 500
+
+    if site is None or code is None:
+        return jsonify('Invalid or missing args'), 400
+
+    if site == 'News':
+        if getAdm() == code or getEditor() == code:
+            return jsonify(True), 200
+        else:
+            return jsonify(False), 200
+
+    if site == 'analytics':
+        if getAdm() == code:
+            return jsonify(True), 200
+        else:
+            return jsonify(False), 200
+
+    return jsonify(False), 200
