58bed190e193e3857922b8ffcc6d1c2d11b60336
AtLaS <atlaso4ek@gmail.com>
2025-10-09 21:32:12 +0700

    Reviews DB methods release

---
+++ src/Database/reviews/DB_reviews.py
@@ -0,0 +1,19 @@
+import datetime
+
+from src.Database.connection import connect
+
+def post_reviews(datetime:datetime.datetime,
+                 text:str):
+    conn, cur = connect()
+
+    if datetime is None or text is None:
+        return False
+
+    query = 'INSERT INTO private.tgreviews (datetime, text) VALUES (%s, %s);'
+    data = (datetime, text)
+
+    cur.execute(query, data)
+    conn.commit()
+
+    cur.close()
+    conn.close()
