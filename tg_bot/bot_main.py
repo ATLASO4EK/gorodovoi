0d86273488a703722a4f2a299a903d6f0920c25d
1Evgesha1 <vedernikovartem760@gmail.com>
2025-10-12 14:04:37 +0700

    Add files via upload

--- tg_bot/bot_main.py
+++ tg_bot/bot_main.py
@@ -1,4 +1,8 @@
 import asyncio
+import threading
+from ctypes.wintypes import tagRECT
+
+from sqlalchemy.sql.operators import truediv
 from telebot import asyncio_filters
 from callback_data import *
 import schedule, time, requests, json
@@ -15,7 +19,6 @@ bot.setup_middleware(StateMiddleware(bot))
 
 # Start polling
 import asyncio
-
 @bot.message_handler(commands=['start'])
 async def AddNotif(message, state: StateContext,):
     async with state.data() as data:
@@ -29,27 +32,28 @@ async def AddNotif(message, state: StateContext,):
                                    chat_id=message.chat.id)
             return
 
-    url = 'http://127.0.0.1:8000/api/v1/tg'
+    UrlTg = 'http://127.0.0.1:8000/api/v1/tg'
+    UrlJams = 'http://127.0.0.1:8000/api/v1/jams'
+
     params = {}
     params['isnotifon']=True
-    resp = json.loads(requests.get(url=url, params=params).content.decode('utf-8'))
+    resp = json.loads(requests.get(url=UrlTg, params=params).content.decode('utf-8'))
+    jams_data = json.loads(requests.get(UrlJams).content.decode('utf-8'))
+
     IdTg = np.asarray(resp)
     IdTg=list(map(lambda x: x[0], IdTg))
     print(IdTg)
 
     if userid in IdTg:
-        #сюда надо бы с пробок выводить инфу, но бд не робит
-        bot.send_message(message.chat.id,text="текст оповещения")
+        if jams_data[:][1]>=3:
+            await bot.send_message(message.chat.id, text= 'text')
     elif not userid in IdTg:
         await auth(message, state)
         await bot.send_message(text='Произведена новая регистрация', chat_id=chatid)
-        
 
-
-schedule.every().hour.do(AddNotif)
-
-schedule.run_pending()
-time.sleep(1)
 
 print('bot started')
 asyncio.run(bot.polling())
+
+
+
