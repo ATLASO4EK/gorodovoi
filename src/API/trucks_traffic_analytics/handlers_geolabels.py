5b51cf3b22b9fea9b465e9a18b9613a15af5b887
AtLaS <atlaso4ek@gmail.com>
2025-10-16 19:17:03 +0700

    geolabels get methods release

--- src/API/trucks_traffic_analytics/handlers_geolabels.py
+++ src/API/trucks_traffic_analytics/handlers_geolabels.py
@@ -7,7 +7,21 @@ from src.Database.trucks_traffic.DB_trucks_traffic import get_detectors, post_de
 @app.route('/api/v1/trucks_traffic/detectors')
 def get_detectors_api():
     try:
-        name = 11
+        name = str(request.args.get('name')) if request.args.get('name') is not None else None
     except Exception as e:
         print(e)
         return jsonify(str(e)), 400
+
+    try:
+        suc, data = get_detectors(name=name)
+
+        if suc:
+            return jsonify(data), 200
+        elif not suc:
+            return jsonify(str(data)), 500
+        else:
+            return jsonify('Unexpected error occurred'), 500
+
+    except Exception as e:
+        print(e)
+        return jsonify(str(e)), 500
